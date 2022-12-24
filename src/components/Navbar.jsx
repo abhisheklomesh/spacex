e7f90254edcfa8ae9212430684afe97dc49b1ff1
Abhishek Lomesh <abhishek.lomesh45@gmail.com>
2022-12-24 14:57:27 +0530

    navbar responsive design completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,73 @@
 import Logo from "../images/logo.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <nav className="flex justify-between px-2 py-2 bg-neutral-900">
-      <img className="w-64" src={Logo} alt="logo"></img>
-      <div className="flex gap-5 mr-4 text-xl text-gray-300 ">
-        <Link to="/">
-          <span className="hover:text-stone-100">Home</span>
-        </Link>
-        <Link to="/history">
-          <span className="hover:text-stone-100">History</span>
-        </Link>
-        <Link to="/payloads">
-          <span className="hover:text-stone-100">Payloads</span>
-        </Link>
-      </div>
-    </nav>
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-1 py-2 bg-neutral-900 ">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              to="/"
+            >
+              <img className="w-64" src={Logo} alt="logo"></img>
+            </Link>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <span class="sr-only">Open main menu</span>
+              <svg
+                class="w-10 h-10"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul
+              className="flex flex-col lg:flex-row list-none lg:ml-auto "
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+              <li className="hover:opacity-50">
+                <Link
+                  to="/history"
+                  className="px-3 py-2 flex items-center text-2xl  font-bold leading-snug text-white hover:opacity-75"
+                >
+                  History
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-2xl  font-bold leading-snug text-white hover:opacity-75"
+                  to="/payloads"
+                >
+                  Payloads
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 };
 
